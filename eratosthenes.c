a59632110c334a55f3c7c1762ec62be91d75ea71
Rastislav Uhliar <void@Sentinel>
2024-03-23 18:08:27 +0100

    asdf

--- eratosthenes.c
+++ eratosthenes.c
@@ -11,20 +11,16 @@
 
 void Eratosthenes(bitset_t pole) {
     bitset_fill(pole, 1);
-   // bitset_setbit(pole, 0, 0);
+    bitset_setbit(pole, 0, 0);
     bitset_setbit(pole, 1, 0);
-    bitset_setbit(pole, 2, 0);
-    // bitset_setbit(pole, 3, 0);
-    // bitset_setbit(pole, 6, 0);
-    //bitset_index_t index = 2;
-   // bitset_index_t N = sqrt(pole[0]);
+    bitset_index_t N = sqrt(pole[0]);
 
-    // for (bitset_index_t i = 2; i < N; i++) {
-    //     if (bitset_getbit(pole, i)) {
-    //         for (bitset_index_t j = i * i; j < pole[0]; j += i) {
-    //             bitset_setbit(pole, j, 0);
-    //         }
-    //     }
-    // }
+    for (bitset_index_t index = 2; index < N; index++) {
+        if (bitset_getbit(pole, index)) {
+            for (bitset_index_t j = index * index; j < pole[0]; j += index) {
+                bitset_setbit(pole, j, 0);
+            }
+        }
+    }
 
 }
--- primes.c
+++ primes.c
@@ -10,18 +10,18 @@
 #include "error.h"
 
 int main(void) {
-    bitset_create(pole, 10);
+    bitset_create(pole, 1000);
     Eratosthenes(pole);
 
-    // int count=0;
-    // for (bitset_index_t i = 10 - 1; i > 1 && count < 10; --i) {
-    //     if (bitset_getbit(pole, i)) {
-    //         printf("%lu\n", i);
-    //         ++count;
-    //     }
-    // }
-    for(bitset_index_t i = 0; i < 10; i++){
-            printf("%lu : %u\n",i, bitset_getbit(pole, i) ? 1 : 0);
+    int count=0;
+    for (bitset_index_t i = 1000-1; i > 1 && count < 1000; --i) {
+        if (bitset_getbit(pole, i)) {
+            printf("%lu\n", i);
+            ++count;
+        }
     }
+    // for(bitset_index_t i = 0; i < 10; i++){
+    //         printf("%lu : %u\n",i, bitset_getbit(pole, i) ? 1 : 0);
+    // }
     return 0;
 }
