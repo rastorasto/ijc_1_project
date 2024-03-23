a40ae08d8f770aaec56321653dbf9d902113cc9e
Rastislav Uhliar <void@Sentinel>
2024-03-23 14:24:25 +0100

    added erat..

---
+++ eratosthenes.h
@@ -0,0 +1,16 @@
+// eratosthenes.h
+// Řešení IJC-DU1, příklad a), 20.3.2111
+// Autor: Rastislav Uhliar, FIT
+// Přeloženo: gcc 10.2
+// ...popis příkladu - poznámky, omezení, atd
+
+#ifndef ERATOSTHENES_H
+#define ERATOSTHENES_H
+
+#include "error.h"
+#include "bitset.h"
+#include <math.h>
+
+void Eratosthenes(bitset_t pole);
+
+#endif
